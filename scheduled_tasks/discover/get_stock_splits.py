727c6dfba56fe05fe3fa68409c3ae5d8f64b7175
spartan737 <tanguanquan@yahoo.com>
2022-04-06 22:13:30 +0800

    add web link for dividends and stock split

--- scheduled_tasks/discover/get_stock_splits.py
+++ scheduled_tasks/discover/get_stock_splits.py
@@ -2,6 +2,7 @@ import os
 import sys
 import yaml
 import requests
+import numpy as np
 import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from helpers import connect_mysql_database
@@ -23,7 +24,13 @@ def main():
         final_df = final_df.append(df)
 
     final_df.drop_duplicates(inplace=True)
+    final_df["ratio"] = final_df["split_to"] / final_df["split_from"]
+    final_df['split_to'] = np.where(final_df['split_from'] > 100, final_df['ratio'], final_df['split_to'])
+    final_df['split_from'] = np.where(final_df['split_from'] > 100, 1, final_df['split_from'])
+    del final_df["ratio"]
+    final_df = final_df.round(3)
     print(final_df)
+
     cur.executemany("INSERT IGNORE INTO stock_splits VALUES (%s ,%s ,%s ,%s, %s)", final_df.values.tolist())
     cnx.commit()
 
--- static/javascript/discover/stock_split.js
+++ static/javascript/discover/stock_split.js
@@ -1,3 +1,28 @@
+function load_table() {
+    trs = document.querySelectorAll("tr")
+    trs[0].querySelectorAll("th")[4].style.display = "none"
+    for (i=1; i<trs.length; i++) {
+        td = trs[i].querySelectorAll("td")
+        td[3].innerHTML = `<b><a href="/ticker?quote=${td[3].innerHTML}">${td[3].innerHTML}</a></b>`
+        td[4].style.display = "none"
+    }
+}
+
+function filter_table(elem) {
+    elem = elem.value
+    trs = document.querySelectorAll("tr")
+    for (i=1; i<trs.length; i++) {
+        tr = trs[i]
+        td = tr.querySelectorAll("td")
+        if (td[4].innerHTML == elem || elem == "All") {
+            tr.classList.remove("filter")
+        }
+        else {
+            tr.classList.add("filter")
+        }
+    }
+}
+
 const searchTicker = (elem) =>{
     let filter = elem.value.toUpperCase();
     let filter_table = elem.parentElement.parentElement.querySelector("table");

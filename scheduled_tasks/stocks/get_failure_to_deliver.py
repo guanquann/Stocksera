9421bb81708cdffaf7bff0e42304ba91c291daf3
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-08-17 11:09:40 +0800

    Fix FTD

--- scheduled_tasks/stocks/get_failure_to_deliver.py
+++ scheduled_tasks/stocks/get_failure_to_deliver.py
@@ -1,11 +1,12 @@
+import io
 import os
 import sys
 import shutil
 import requests
+import zipfile
 import pandas as pd
 from datetime import timedelta
 from pathlib import Path
-from bs4 import BeautifulSoup
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
 from helpers import connect_mysql_database
@@ -24,47 +25,86 @@ def download_ftd():
     shutil.rmtree("database/failure_to_deliver/csv")
     os.mkdir("database/failure_to_deliver/csv")
 
-    base_url = "https://www.sec.gov"
+    headers = {
+        "User-Agent": "stocksera stocksera@stocksera.com",
+        "Accept-Encoding": "gzip, deflate",
+        "Host": "www.sec.gov",
+    }
 
-    text_soup_high_short_interested_stocks = BeautifulSoup(
-        requests.get("https://www.sec.gov/data/foiadocsfailsdatahtm").text, "lxml")
+    data = requests.get(
+        "https://www.sec.gov/data.json",
+        headers=headers,
+    ).json()["dataset"]
 
-    print(text_soup_high_short_interested_stocks, "!!!!!")
+    ftd_idx = None
+    for idx, d in enumerate(data):
+        if "title" in d and d["title"] == "Fails-to-Deliver Data":
+            ftd_idx = idx
+            break
 
-    ftd_url_links = text_soup_high_short_interested_stocks.findAll("table")[1].findAll("a")
-    for url_link in ftd_url_links[:24]:
-        df = pd.read_csv(base_url + url_link["href"], delimiter="|", error_bad_lines=False, encoding="cp1252")
-        df.to_csv("database/failure_to_deliver/csv/" + url_link["href"].split("/")[-1].replace(".zip", ".csv"),
-                  index=None)
+    if ftd_idx:
+        fails = data[ftd_idx]["distribution"]
+
+        for fail in fails[:24]:
+            url = fail["downloadURL"]
+
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                with zipfile.ZipFile(io.BytesIO(response.content)) as the_zip:
+                    csv_filename = the_zip.namelist()[0]
+                    with the_zip.open(csv_filename) as csv_file:
+                        df = pd.read_csv(
+                            csv_file, encoding="cp1252", sep="|", on_bad_lines="skip"
+                        )
+                        print(df.head())
+
+                        df.to_csv(
+                            "database/failure_to_deliver/csv/"
+                            + url.split("/")[-1].replace(".zip", ".csv"),
+                            index=None,
+                        )
 
 
 def upload_to_df(files_available, limit=24):
     """
     Combine all the 1/2 monthly csv into 1 large csv file
     """
-    combined_df = pd.DataFrame(columns=["SETTLEMENT DATE", "SYMBOL", "QUANTITY (FAILS)", "PRICE"])
+    combined_df = pd.DataFrame(
+        columns=["SETTLEMENT DATE", "SYMBOL", "QUANTITY (FAILS)", "PRICE"]
+    )
     for file in files_available[:limit]:
         df = pd.read_csv(file)
         del df["CUSIP"]
         del df["DESCRIPTION"]
-        df["SETTLEMENT DATE"] = df["SETTLEMENT DATE"].apply(lambda x: str(x[:4] + "-" + x[4:6] + "-" + x[6:]))
-        combined_df = combined_df.append(df).drop_duplicates()
+        df["SETTLEMENT DATE"] = df["SETTLEMENT DATE"].apply(
+            lambda x: str(x[:4] + "-" + x[4:6] + "-" + x[6:])
+        )
+        combined_df = pd.concat([combined_df, df]).drop_duplicates()
 
     combined_df["SETTLEMENT DATE"] = combined_df["SETTLEMENT DATE"].astype(str)
-    combined_df.rename(columns={"SETTLEMENT DATE": "Date",
-                                "SYMBOL": "Ticker",
-                                "QUANTITY (FAILS)": "Failure to Deliver",
-                                "PRICE": "Price"}, inplace=True)
-    combined_df["T+35 Date"] = pd.to_datetime(combined_df['Date'], format='%Y-%m-%d', errors="coerce") + timedelta(days=35)
+    combined_df.rename(
+        columns={
+            "SETTLEMENT DATE": "Date",
+            "SYMBOL": "Ticker",
+            "QUANTITY (FAILS)": "Failure to Deliver",
+            "PRICE": "Price",
+        },
+        inplace=True,
+    )
+    combined_df["T+35 Date"] = pd.to_datetime(
+        combined_df["Date"], format="%Y-%m-%d", errors="coerce"
+    ) + timedelta(days=35)
     combined_df["T+35 Date"] = combined_df["T+35 Date"].astype(str)
     combined_df = combined_df[~combined_df["Ticker"].isna()]
     combined_df.sort_values(by="Date", inplace=True)
-    print(combined_df)
 
     start = 0
     while start < len(combined_df):
-        print(start)
-        cur.executemany("INSERT IGNORE INTO ftd VALUES (%s, %s, %s, %s, %s)", combined_df[start:start+50000].values.tolist())
+        cur.executemany(
+            "INSERT IGNORE INTO ftd VALUES (%s, %s, %s, %s, %s)",
+            combined_df[start : start + 50000].values.tolist(),
+        )
         cnx.commit()
         start += 50000
 
@@ -76,7 +116,9 @@ def get_top_ftd(filename):
     """
     df = pd.read_csv(filename)
     df.sort_values(by=["SYMBOL", "SETTLEMENT DATE"], ascending=False, inplace=True)
-    df["PRICE"] = df["PRICE"].apply(lambda x: pd.to_numeric(x, errors='coerce')).fillna(0)
+    df["PRICE"] = (
+        df["PRICE"].apply(lambda x: pd.to_numeric(x, errors="coerce")).fillna(0)
+    )
 
     original_df = df.copy()
 
@@ -91,25 +133,42 @@ def get_top_ftd(filename):
     del original_df["CUSIP"]
     del original_df["DESCRIPTION"]
 
-    original_df["SETTLEMENT DATE"] = original_df["SETTLEMENT DATE"].apply(lambda x: str(x[:4] + "-" + x[4:6] + "-" + x[6:]))
+    original_df["SETTLEMENT DATE"] = original_df["SETTLEMENT DATE"].apply(
+        lambda x: str(x[:4] + "-" + x[4:6] + "-" + x[6:])
+    )
     original_df["SETTLEMENT DATE"] = original_df["SETTLEMENT DATE"].astype(str)
-    original_df["FTD x $"] = (original_df["QUANTITY (FAILS)"].astype(int) * original_df["PRICE"].astype(float)).astype(int)
-    original_df.rename(columns={"SETTLEMENT DATE": "Date",
-                                "SYMBOL": "Ticker",
-                                "QUANTITY (FAILS)": "FTD",
-                                "PRICE": "Price"}, inplace=True)
-    original_df["T+35 Date"] = pd.to_datetime(original_df['Date'], format='%Y-%m-%d', errors="coerce") + timedelta(
-        days=35)
-    original_df["T+35 Date"] = original_df["T+35 Date"].astype(str)  # .apply(lambda x: x.replace("-", "/"))
+    original_df["FTD x $"] = (
+        original_df["QUANTITY (FAILS)"].astype(int) * original_df["PRICE"].astype(float)
+    ).astype(int)
+    original_df.rename(
+        columns={
+            "SETTLEMENT DATE": "Date",
+            "SYMBOL": "Ticker",
+            "QUANTITY (FAILS)": "FTD",
+            "PRICE": "Price",
+        },
+        inplace=True,
+    )
+    original_df["T+35 Date"] = pd.to_datetime(
+        original_df["Date"], format="%Y-%m-%d", errors="coerce"
+    ) + timedelta(days=35)
+    original_df["T+35 Date"] = original_df["T+35 Date"].astype(str)
 
     # Sort df based on number of FTD days that meet criteria and add new row between tickers
-    combined_df = pd.DataFrame(columns=["Date", "Ticker", "FTD", "Price", "FTD x $"])
+    combined_df = pd.DataFrame(
+        columns=["Date", "Ticker", "FTD", "Price", "FTD x $", "T+35 Date"]
+    )
     for i in df["SYMBOL"].value_counts().index:
         individual_df = original_df[original_df["Ticker"] == i]
-        individual_df = individual_df.append([""])
-        combined_df = combined_df.append(individual_df)
-    del combined_df[0]
-    combined_df.to_sql('top_ftd', engine, if_exists="replace", method="multi", index=False)
+        print(individual_df)
+        individual_df = pd.concat(
+            [individual_df, pd.DataFrame([[""] * 6], columns=individual_df.columns)]
+        )
+        combined_df = pd.concat([combined_df, individual_df])
+
+    combined_df.to_sql(
+        "top_ftd", engine, if_exists="replace", method="multi", index=False
+    )
 
 
 def main():
@@ -122,5 +181,5 @@ def main():
     print("FTD Successfully Completed...\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
